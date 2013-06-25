c4696ac346b589b66baa441e323599a0cadb866c
Jon M. Wear <jon.m.wear@gmail.com>
2013-06-24 22:47:53 -0400

    more test scenario updates.
    Really learning a lot by doing this.

--- DapperHowToData/ExamplesForIEnumerable.cs
+++ DapperHowToData/ExamplesForIEnumerable.cs
@@ -23,7 +23,7 @@ namespace DapperHowToData
             Dictionary<int, Player> dictionaryexample = cn.Query<Player>(queryteamID2).ToDictionary(p => p.PlayerID, p => p);
 
             //reference type
-            Player firstplayer = cn.Query<Player>("select * from player where playerid=1").First();
+            Player firstplayer = cn.Query<Player>("select * from player where teamID=1").First();
 
             string queryplayerIDNull = "select * from player where playerid=100";
             try
@@ -32,19 +32,31 @@ namespace DapperHowToData
             }
             catch (InvalidOperationException invalidopex)
             {
+                //Notice that we didn't catch Execption ex, we narrowed down the type
+                //to the specific kind of exception that could happen.
+                //If at all possible, narrow down your exception handling.
                 Console.WriteLine(string.Format("Error! {0}", invalidopex.Message));
             }
 
             Player nofirstplayerdefault = cn.Query<Player>(queryplayerIDNull).FirstOrDefault();
 
             string queryplayerNameNull = "select playername from player where playerID = 100";
-            string firstplayername = cn.Query<string>(queryplayerNameNull).FirstOrDefault();
 
+            Player playerdoesnotexist = new Player();
+            playerdoesnotexist.PlayerID = -1;
+            playerdoesnotexist.PlayerName = "No such player.";
+            playerdoesnotexist.TeamID = -1;
+            
+            Player firstplayernalt = cn.Query<Player>(queryplayerNameNull).DefaultIfEmpty(playerdoesnotexist).FirstOrDefault();
+            
+            string firstplayername = cn.Query<string>(queryplayerNameNull).FirstOrDefault();
             try
             {
                 //Why doesn't this work?
                 string firstplayernamepropr = cn.Query<Player>(queryplayerNameNull).FirstOrDefault().PlayerName;
-                //Because we are first returning the default, which is null, which has no value;
+                //Because we are first returning the default, which is null, which has no values for property names
+                //even though the default value of the PlayerName property is null, it can't get that far because
+                //the object that it is attached to is null.  There's no there there.;
             }
             catch (NullReferenceException exnull)
             {
