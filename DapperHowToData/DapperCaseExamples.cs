d64631640e52c8b001ec62b4160c67a2409915a1
Jon M. Wear <jon.m.wear@gmail.com>
2013-07-18 00:15:52 -0400

    Tests for Dynamic vs Type data calls

---
+++ DapperHowToData/DapperCaseExamples.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlServerCe;
+using DapperHowToData.POCOs;
+using System.Reflection;
+using System.Dynamic;
+using System.Linq;
+namespace DapperHowToData
+{
+    public class DapperCaseExamples
+    {
+        #region "DataConnection"
+        private static string ConnString = Properties.Settings.Default.DapperDataConnectionString;
+
+        private class DataConnection : IDisposable
+        {
+            SqlCeConnection cn;
+
+            public DataConnection(string cnstring)
+            {
+                cn = new SqlCeConnection(cnstring);
+            }
+            public SqlCeConnection Connection
+            {
+                get
+                {
+                    return this.cn;
+                }
+                private set { }
+            }
+            void IDisposable.Dispose()
+            {
+                if (cn.State != System.Data.ConnectionState.Closed)
+                {
+                    cn.Close();
+                }
+            }
+        }
+        #endregion
+
+        public static Player Return_A_PlayerType_ByID(int playerID)
+        {
+            Player result = new Player();
+            using (DataConnection dc = new DataConnection(Properties.Settings.Default.DapperDataConnectionString))
+            {
+                result = dc.Connection.Query<Player>("select * from player where playerID = @playerID", new { playerID = playerID }).FirstOrDefault();
+            }
+            return result;
+        }
+        public static dynamic Return_A_PlayerDynamic_ByID(int playerID)
+        {
+            dynamic result;
+            using (DataConnection dc = new DataConnection(Properties.Settings.Default.DapperDataConnectionString))
+            {
+                result = dc.Connection.Query("select * from player where playerID = @playerID", new { playerID = playerID }).FirstOrDefault();
+            }
+            return result;
+        }
+    }
+}
---
+++ DapperHowToTests/CaseTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using DapperHowToData;
+using DapperHowToData.POCOs;
+using System.Dynamic;
+namespace DapperHowToTests
+{
+    [TestClass]
+    public class CaseTests
+    {
+        [TestMethod,TestCategory("Case Tests")]
+        public void Get_Player_Type()
+        {
+           var result  =  DapperCaseExamples.Return_A_PlayerType_ByID(1);
+           Assert.IsTrue(result.GetType() ==typeof(Player));
+        }
+
+        [TestMethod, TestCategory("Case Tests")]
+        public void Get_Player_Dynamic_Expando()
+        {
+            var result = DapperCaseExamples.Return_A_PlayerType_ByID(1);
+            dynamic dynPlayer = new ExpandoObject();
+            dynPlayer.playerID = result.PlayerID;
+            Assert.AreEqual(1, dynPlayer.playerID);
+        }
+
+        [TestMethod, TestCategory("Case Tests")]
+        public void Get_Player_Dynamic()
+        {
+            var result = DapperCaseExamples.Return_A_PlayerDynamic_ByID(1);
+            Assert.AreEqual(1, result.playerID);
+        }
+
+        [TestMethod, TestCategory("Case Tests")]
+        [ExpectedException(typeof(System.InvalidCastException),
+        "The returned object type doesn't match the expected type.")]
+        public void GetPlayer_Dynamic_Match_Player_Type()
+        {
+            var result = DapperCaseExamples.Return_A_PlayerDynamic_ByID(1);
+            Player player = (Player)result;
+   
+        }
+
+     
+        [TestMethod, TestCategory("Case Tests")]
+        [ExpectedException(typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException),
+        "Could not match up class property with object property.")]
+        public void GetPlayer_Dynamic_Match_Player_Property_Invalid()
+        {
+            var result = DapperCaseExamples.Return_A_PlayerDynamic_ByID(1);
+            Player player = new Player();
+            player.PlayerID = result.PlayerID;
+
+        }
+
+        [TestMethod, TestCategory("Case Tests")]
+        public void GetPlayer_Dynamic_Match_Player_Property()
+        {
+            var result = DapperCaseExamples.Return_A_PlayerDynamic_ByID(1);
+            Player player = new Player();
+            player.PlayerID = result.playerID;
+            Assert.IsTrue(player.PlayerID == 1);
+        }
+
+    }
+}
